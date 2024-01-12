a9f97c82d567d4e34b8748b7139708f85d604217
TanishqDsharma <tanishqsharma612@gmail.com>
2024-01-11 23:05:32 -0800

    Commit

---
+++ components/MovieList.tsx
@@ -0,0 +1,34 @@
+import { Card } from './Card'
+import { FC, useEffect, useState } from 'react'
+import { Movie } from '../models/Movie'
+import * as web3 from "@solana/web3.js"
+import { useConnection, useWallet } from '@solana/wallet-adapter-react'
+
+const MOVIE_REVIEW_PROGRAM_ID = 'CenYq6bDRB7p73EjsPEpiYN7uveyPUTdXkDkgUduboaN'
+
+export const MovieList: FC = () => {
+    const [movies, setMovies] = useState<Movie[]>([])
+    const {connection} = useConnection()
+
+    useEffect(() => {
+        connection.getProgramAccounts(new web3.PublicKey(MOVIE_REVIEW_PROGRAM_ID)).then(async (accounts) => {
+          const movies: Movie[] = accounts.map(({ account }) => {
+            return Movie.deserialize(account.data)
+          })
+    
+          setMovies(movies)
+        })
+      }, [])
+    
+    return (
+        <div>
+            {
+                movies.map((movie, i) => {
+                    return (
+                        <Card key={i} movie={movie} />
+                    )
+                })
+            }
+        </div>
+    )
+}
